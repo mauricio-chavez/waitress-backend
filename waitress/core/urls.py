857c166c98a49ca135a4d7d4626d770ad9d9dc9f
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2019-07-18 21:13:54 -0500

    Formatted fake index page

--- waitress/core/urls.py
+++ waitress/core/urls.py
@@ -3,11 +3,13 @@
 from django.urls import path
 from django.http import JsonResponse
 
+
 def index(request):
     """Example page"""
     return JsonResponse({
         "message": "Yes, I'm working, bitch"
     })
+
 
 urlpatterns = [
     path('', index, name='index'),
